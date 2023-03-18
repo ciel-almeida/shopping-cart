ad40b3a31c19d72c0909c1496c3c028eeba9b866
Cleverton Almeida <clevertoncas@gmail.com>
2023-03-18 05:51:15 -0300

    correct shopping cart grid

--- src/components/cart/CartSummary.jsx
+++ src/components/cart/CartSummary.jsx
@@ -9,19 +9,26 @@ function CartSummary() {
 
 	const totalPrice = cart.reduce((sum, item) => sum + item.qty * item.price, 0);
 	const qtyInCart = cart.reduce((sum, item) => sum + item.qty, 0);
+
 	return (
-		<div className="cart-summary">
-			<h2>Summary</h2>
-			<p className="cart-summary__item">
-				Items: <b>{qtyInCart}</b>
-			</p>
-			<p className="cart-summary__item">
-				Total: <b>$ {totalPrice.toFixed(2)}</b>
-			</p>
-			<button className="cart-summary__btn">
-				Payment <AiOutlineArrowRight />
-			</button>
-		</div>
+		<>
+			{cart.length ? (
+				<div className="cart-summary">
+					<h2>Summary</h2>
+					<p className="cart-summary__item">
+						Items: <b>{qtyInCart}</b>
+					</p>
+					<p className="cart-summary__item">
+						Total: <b>$ {totalPrice.toFixed(2)}</b>
+					</p>
+					<button className="cart-summary__btn">
+						Payment <AiOutlineArrowRight />
+					</button>
+				</div>
+			) : (
+				''
+			)}
+		</>
 	);
 }
 
